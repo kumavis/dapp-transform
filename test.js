18b45b78866420b9838bacf0ce97990e59c95117
kumavis <aaron@kumavis.me>
2015-08-25 13:32:15 -0700

    clean - create example dir, deprecate old tests

--- test.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-const DappTransform = require('./index.js')
-
-// transform
-var dappTransform = DappTransform({ origin: 'https://yummyy.am/toothpaste/' })
-
-fs.createReadStream('./test/remote-js.html')
-.pipe(dappTransform)
-.pipe(process.stdout)
